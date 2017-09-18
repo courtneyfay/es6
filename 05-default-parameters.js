3122da7bf16a641094fa6325b0a24684cef69c02
Courtney Fay <fay.courtney@gmail.com>
2017-09-18 11:01:22 -0600

    answers questions 4 and 5

---
+++ 05-default-parameters.js
@@ -0,0 +1,18 @@
+  /*
+   * Write a function that takes 3 parameters:
+   * greeting, firstName, and lastName
+   *
+   * Set the default values to 'hello', your first and last name,
+   * if no input is provided
+   */
+   
+   //const greeting =
+   // console.log(greeting());
+   // console.log(greeting('hi', 'harry', 'hedger'));
+
+   function greeting (greeting = "hello", firstName = "Court", lastName = "Fay") {
+      console.log(greeting + ", " + firstName + " " + lastName);
+   };
+
+   greeting();
+
